8471453752d1d9a392b70c5ed73f8ada216dc5fb
Kuldeep Chahar <kuldeepchahar426@gmail.com>
2023-12-26 15:51:08 -0800

    dfd

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://wizemart-backend.onrender.com";
 // export const baseUrl = "https://clownfish-app-9mjjf.ondigitalocean.app";
 export const web_title = "WizeMart Store - One Stop Shop!!";
 export const app_download_link = "https://play.google.com/store/apps/details?id=com.wizemart_app";
